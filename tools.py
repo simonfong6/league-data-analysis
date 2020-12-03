fb2441a1769506d8537344f81b101f14b74f8392
Simon <simonfong6@gmail.com>
2020-12-02 22:03:25 -0800

    Data

--- tools.py
+++ tools.py
@@ -1,3 +1,26 @@
 """
 Various tools and functions to operate on the data which is reusable.
 """
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+DATA_DIRECTORY = './data'
+
+
+def data_path(path):
+    full_path = os.path.join(DATA_DIRECTORY, path)
+    return full_path
+
+
+def list_data_files():
+    data_files = os.listdir(DATA_DIRECTORY)
+    data_files.sort()
+    return data_files
+
+def load(name):
+    path = data_path(name)
+    df = pd.read_pickle(name)
